ddb2acde7d6c6370578ed166c0b1e14548025e5d
Andres Felipe Navas Alvear <136179236+craftube23@users.noreply.github.com>
2025-08-13 19:55:40 -0500

    creacion de l menu prinsipal

--- banco.py
+++ banco.py
@@ -1,9 +1,33 @@
-def menu_principal():
-    menu ='''
-        menu
-        '''
-    return  menu
-while True:
-    print(menu_principal())
-    break
-    
+def menu():
+    while True:
+        print("""
+        ====== Sistema de Gestión de Cuentas Bancarias ======
+        1. Crear Cuenta
+        2. Depositar Dinero
+        3. Solicitar Crédito
+        4. Retirar Dinero
+        5. Pago Cuota Crédito
+        6. Cancelar Cuenta
+        7. Salir
+        """)
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            print()
+        elif opcion == "2":
+            print()
+        elif opcion == "3":
+            print()
+        elif opcion == "4":
+            print()
+        elif opcion == "5":
+            print()
+        elif opcion == "6":
+            print()
+        elif opcion == "7":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Ingrese una opcion valida.")
+
+menu()
